eda8463ebb8c139ef6d12f84731bc02ef8bb1f0e
walid taib <walidtaib44@gmail.com>
2023-02-04 10:19:56 +0100

    update

---
+++ routes/companies.js
@@ -0,0 +1,33 @@
+const express =require('express');
+const body_parser=require('body-parser');
+const companies=express.Router();
+const User=require('../models/company');
+companies.use(body_parser.json())
+companies.route('/')
+var passport = require('passport');
+
+companies.post('/signup', (req, res, next) => {
+    User.register(new User({username: req.body.username}), 
+      req.body.password, (err, user) => {
+      if(err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({err: err});
+      }
+      else {
+        passport.authenticate('local')(req, res, () => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({success: true, status: 'Registration Successful!'});
+        });
+      }
+    });
+  });
+  
+  companies.post('/login', passport.authenticate('local'), (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: true, status: 'You are successfully logged in!'});
+  });
+
+module.exports=companies;
